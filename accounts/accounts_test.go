f363304af6f29dac3fe0bb5095523204d3f8bfad
Douglas Santos <hiperhavy_doug@hotmail.com>
2022-08-08 23:13:32 -0300

    test: create account with parent

--- accounts/accounts_test.go
+++ accounts/accounts_test.go
@@ -27,4 +27,25 @@ func TestAccount(t *testing.T) {
 			t.Error("Should have an ID")
 		}
 	})
+
+	t.Run("With Parent", func(t *testing.T) {
+		parent, _ := accounts.Create("Assets", accounts.Asset, nil)
+		cash, err := accounts.Create("Cash", accounts.Asset, parent)
+
+		if err != nil {
+			t.Error(err)
+		}
+
+		if cash.ID != 3 {
+			t.Errorf("Expected ID 3, got %v", cash.ID)
+		}
+
+		if cash.ParentID != 2 {
+			t.Errorf("Expected ParentID to be 2, got %v", cash.ParentID)
+		}
+
+		if cash.Parent == nil {
+			t.Error("Should have a parent")
+		}
+	})
 }
